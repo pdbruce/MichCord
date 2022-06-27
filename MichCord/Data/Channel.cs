a2af8802aa0fc30d456d4862910b347fbea3d7c4
Harrison Barnett <h.alexander.barnett@gmail.com>
2022-06-27 17:46:52 -0400

    refactor, consolidate component library; testing

--- MichCord/Data/Channel.cs
+++ MichCord/Data/Channel.cs
@@ -1,7 +1,8 @@
-﻿namespace MichCord
+﻿namespace MichCord.Data
 {
     public class Channel
     {
         public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }

395974fd819d8d99aaac2abbe22791bf414998da
Harrison Barnett <h.alexander.barnett@gmail.com>
2022-07-07 15:07:59 -0400

    autoscroll on send; styles

---
+++ MichCord/wwwroot/scripts/Helpers.js
@@ -0,0 +1,9 @@
+﻿function handleSendMessage() {
+    var chat = document.querySelector(".message-stream");
+    chat.scroll({
+        top: chat.scrollHeight,
+        behavior: 'smooth'
+    });
+    var input = document.querySelector(".message-input");
+    input.focus();
+}
